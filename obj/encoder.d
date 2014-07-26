fb5b0c247ea4822a71e235458538f958d56be429
kelvinlyan <375898307@qq.com>
2014-07-26 20:41:25 +0800

    client

--- obj/encoder.d
+++
@@ -1,5 +0,0 @@
-obj/encoder.d : src/encoder.cpp include/encoder.h include/msg_json.h \
- include/tools.h
-obj/encoder.o : src/encoder.cpp include/encoder.h include/msg_json.h \
- include/tools.h
-	g++ -c src/encoder.cpp -o obj/encoder.o -Iinclude -lboost_system -lboost_thread -ljsoncpp -ltinyxml
